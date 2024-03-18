9833e5ab7ceede6c652518909a69d97f148bdea4
Kaio <kaio.santiago.13@hotmail.com>
2024-03-18 08:54:50 -0300

    Aprendendo sobre match

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,57 @@
-use std::mem;
+use std::{io::stdin, mem};
 mod sh;
 #[allow(unused_variables)]
 #[warn(unused_unsafe)]
+
+enum State{
+    Locked,
+    Failed,
+    Unlocked
+}
+
+pub fn entry_password(){
+    
+    let code = String::from("1234");
+    let mut state = State::Locked;
+    let mut entry = String::new();
+    loop{
+        match state {
+            State::Locked => {
+                let mut input = String::new();
+                match stdin().read_line(&mut input) {
+                    Ok(_) => {entry.push_str(&input.trim_end());}
+                    Err(_) => continue
+                }
+                if entry == code{
+                    state = State::Unlocked;
+                    continue;
+                }
+                if !code.starts_with(&entry){
+                    state = State::Failed;
+                }
+            },
+            State::Failed => {
+                println!("FAILED");
+                entry.clear();
+                state = State::Locked;
+                continue;
+            },
+            State::Unlocked => {
+                println!("UNLOCKED");
+                break;
+            }
+        }
+    }
+}
+#[allow(unused_variables)]
 fn main() {
     let mut age = 30;
     // criando um ponteiro e apontando para a variável age
     let ptr = &mut age as *mut i32;
     println!("{:?}-{}", ptr, unsafe {*ptr});
+    // Trocando o valor da variável através do ponteiro
     unsafe{*ptr = 29;}    
+    //Usando aritmétrica de ponteiros
     println!("{:?}-{}",  ptr.wrapping_offset(1), unsafe {*ptr});
     let ptr2 = ptr.clone();
     //Unsafe é usado para acessar um valor de um ponteiro
@@ -20,4 +64,7 @@ fn main() {
     let f1 = f64::powf(5.5, 2.0);
     println!("Take up {} bytes", mem::size_of_val(&age));
     sh::stack_and_heap();
+
+    entry_password();
+    
 }
