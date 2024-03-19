61cc0f9c537df748dc5874dde8ef0efb7d195e34
Kaio <kaio.santiago.13@hotmail.com>
2024-03-19 08:25:58 -0300

    mais estudos

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,13 @@
-use std::{io::stdin, mem};
+use std::mem;
 mod sh;
+
+#[allow(unused_imports)]
+use crate::sh::sh::{stack_and_heap, entry_password, IntOrFloat};
+
 #[allow(unused_variables)]
 #[warn(unused_unsafe)]
-
-enum State{
-    Locked,
-    Failed,
-    Unlocked
-}
-
-pub fn entry_password(){
-    
-    let code = String::from("1234");
-    let mut state = State::Locked;
-    let mut entry = String::new();
-    loop{
-        match state {
-            State::Locked => {
-                let mut input = String::new();
-                match stdin().read_line(&mut input) {
-                    Ok(_) => {entry.push_str(&input.trim_end());}
-                    Err(_) => continue
-                }
-                if entry == code{
-                    state = State::Unlocked;
-                    continue;
-                }
-                if !code.starts_with(&entry){
-                    state = State::Failed;
-                }
-            },
-            State::Failed => {
-                println!("FAILED");
-                entry.clear();
-                state = State::Locked;
-                continue;
-            },
-            State::Unlocked => {
-                println!("UNLOCKED");
-                break;
-            }
-        }
-    }
-}
 #[allow(unused_variables)]
+#[allow(unused_mut)]
 fn main() {
     let mut age = 30;
     // criando um ponteiro e apontando para a variável age
@@ -63,8 +27,8 @@ fn main() {
 
     let f1 = f64::powf(5.5, 2.0);
     println!("Take up {} bytes", mem::size_of_val(&age));
-    sh::stack_and_heap();
-
-    entry_password();
-    
+    // stack_and_heap();
+    // entry_password();
+    let mut iof = IntOrFloat{i: 123};
+    println!("{}", unsafe{ iof.i} );
 }
--- src/sh.rs
+++ src/sh.rs
@@ -1,20 +1,68 @@
 #![allow(dead_code)]
-use std::mem;
-
-struct Point
-{
-    x: f64,
-    y: f64
-}
-
-fn origin() -> Point
-{
-   return Point{x: 0.0, y: 0.0}
-}
-#[allow(unused_variables)]
-pub fn stack_and_heap(){
-    let p1 = origin();
-    let p2 = Box::new(origin());
-    println!("p1 takes up {}", mem::size_of_val(&p1));
-    println!("p2 takes up {}", mem::size_of_val(&p2));
+pub mod sh {
+    use std::mem;
+    struct Point
+    {
+        x: f64,
+        y: f64
+    }
+    struct Line{
+        start: Point,
+        end: Point
+    }
+    fn origin() -> Point
+    {
+       return Point{x: 0.0, y: 0.0}
+    }
+    #[allow(unused_variables)]
+    pub fn stack_and_heap(){
+        let p1 = origin();
+        let p2 = Box::new(origin());
+        println!("p1 takes up {}", mem::size_of_val(&p1));
+        println!("p2 takes up {}", mem::size_of_val(&p2));
+    }
+    enum State{
+        Locked,
+        Failed,
+        Unlocked
+    }
+    pub fn entry_password(){
+        
+        let code = String::from("1234");
+        let mut state = State::Locked;
+        let mut entry = String::new();
+        loop{
+            match state {
+                State::Locked => {
+                    let mut input = String::new();
+                    match std::io::stdin().read_line(&mut input) {
+                        Ok(_) => {entry.push_str(&input.trim_end());}
+                        Err(_) => continue
+                    }
+                    if entry == code{
+                        state = State::Unlocked;
+                        continue;
+                    }
+                    if !code.starts_with(&entry){
+                        state = State::Failed;
+                    }
+                },
+                State::Failed => {
+                    println!("FAILED");
+                    entry.clear();
+                    state = State::Locked;
+                    continue;
+                },
+                State::Unlocked => {
+                    println!("UNLOCKED");
+                    break;
+                }
+            }
+        }
+    }
+    // Estrutura que aloca tipos diferentes e memória
+    pub union IntOrFloat{
+        pub i: i32,
+        pub f: f32
+    }
 }
